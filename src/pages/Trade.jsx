56528f45a91b6ed17414dd4e3125ac4bf1dcde55
kajalbabu-msil <kajal.b@marketsimplified.com>
2024-02-27 14:20:25 +0530

    added new components

---
+++ src/pages/Trade.jsx
@@ -0,0 +1,14 @@
+import TradeFilter from "../components/TradeFilter";
+import TradeTable from "../components/TradeTable";
+
+function Trade() {
+
+  return (
+    <div>
+      <TradeFilter/>
+      <TradeTable/>
+    </div>
+  );
+}
+
+export default Trade;
