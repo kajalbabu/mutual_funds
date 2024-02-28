536cca6bf78f099305cb46ada97231f6c58ac3b6
vinay-msil <vinay.a@marketsimplified.com>
2024-02-28 15:46:28 +0530

    added filter by order type and pagination

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,8 +1,18 @@
 import "./Popup.css"
 import { useState } from "react";
 
-export default function Popup(){
+export default function Popup(props){
     const [isVisible, setIsVisible] = useState(true);
+    const [dateComp, setDateComp] = useState(true);
+    const [statusComp, setStatusComp] = useState(false);
+    const handlePopupCategoryDate = ()=>{
+        setDateComp(true);
+        setStatusComp(false);
+    }
+    const handlePopupCategoryStatus = ()=>{
+        setDateComp(false);
+        setStatusComp(true);
+    }
     const hideComponent = () => {
       setIsVisible(!isVisible);
     };
@@ -11,21 +21,40 @@ export default function Popup(){
             {isVisible && <div className="popup-overlay">
                 <div className="popup-content">
                     <div className="filter-popup">
+                        <div className="popupCategory">
+                            <button onClick={handlePopupCategoryDate}>DATE</button>
+                            <button onClick={handlePopupCategoryStatus}>STATUS</button>
+                        </div>
+                            
+                        <div>
+                        {dateComp &&
+                        <div>
                             <h5>Date</h5>
                             <p>Date From</p><input type="date"></input>
                             <p>Date To</p><input type="date"></input>
-                            <h5>Status</h5>
-                            <input id="Completed" type="checkbox" />
-                            <label >Completed</label>
+                        </div>}
 
-                            <input id="Inprogress" type="checkbox" />
-                            <label >Inprogress</label>
-
-                            <input id="Failed" type="checkbox" />
-                            <label>Failed</label>
-                            <br/>
-                            <button className="closeButton" onClick={hideComponent}>Close</button>
+                            {statusComp && <div>
+                                <h5>Status</h5>
+                                <input id="Completed" type="checkbox" />
+                                <label >Completed</label>
+        
+                                <input id="Inprogress" type="checkbox" />
+                                <label >Inprogress</label>
+        
+                                <input id="Failed" type="checkbox" />
+                                <label>Failed</label>
+                            </div>}
+                        </div>
+                            
                     </div>
+                            <div className="cancelAndApplyButtons">
+                                <br/>
+                                <button className="reset-button">RESET</button>
+                                <button className="apply-button">APPLY</button>
+                                <br/>
+                                <button className="closeButton" onClick={hideComponent}>Close</button>
+                            </div>
                 </div>
 
             </div>}
